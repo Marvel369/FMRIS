da83c3f165c7a727d3847757efc0f8bfbb757a48
M Ali Ahsan <ahsan053@umn.edu>
2026-08-06 13:42:09 -0500

    Add market risk feature engineering pipeline

---
+++ src/feature_engineering.py
@@ -0,0 +1,64 @@
+import pandas as pd 
+import numpy as np
+
+from preprocessing import preprocessing_data
+
+def create_features():
+
+    #load
+    df = preprocessing_data()
+
+
+    # feature 1 Moving average 30 days
+    df['sp500_MA_30'] = (df['sp500']
+    .rolling(window=30)
+    .mean()
+    )
+
+    # feature 2 Moving average 90 days for longer trend
+    df['sp500_MA_90'] = (df['sp500']
+        .rolling(window=90)
+        .mean()
+    )
+
+    # feature 3 All time high value until today
+    df['peak_price'] = (df['sp500']
+        .cummax()
+    )
+
+
+    # feature 4 how much down from peak (current - peak) / peak
+    df['drawdown'] = (df['sp500'] - df['peak_price']) / df['peak_price']
+
+    #feature 5 vix % change
+    df['vix_change'] =(
+        df['vix']
+        .pct_change()
+    )
+
+    #dropping nan values
+    df = df.dropna()
+
+    #save
+    df.to_csv(
+    "data/features.csv",
+    index=False
+)
+
+    return df
+
+if __name__ == "__main__":
+
+    data = create_features()
+
+    print(data.head(30))
+    print(data.tail())
+    print(data.shape)
+
+
+
+
+
+
+
+
